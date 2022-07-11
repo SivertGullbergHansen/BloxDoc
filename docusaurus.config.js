1180229a25e1619a940028ccbc5438604d9ca4f3
sivertgullberghansen <sivertgh@stud.ntnu.no>
2022-07-11 13:28:06 +0200

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -81,15 +81,6 @@ const config = {
         style: "dark",
         links: [
           {
-            title: "Docs",
-            items: [
-              {
-                label: "Docs",
-                to: "/docs/intro",
-              },
-            ],
-          },
-          {
             title: "Community",
             items: [
               {
